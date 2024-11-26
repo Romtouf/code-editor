99d8b94056aeb2bbe1b90a171635f5d86dffb589
Romtouf <romtouf@laposte.net>
2024-11-26 23:34:02 +0100

    maj

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 // script.js
 
-// Ajoutez ce code au début du fichier
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(registration => {
-        console.log('ServiceWorker enregistré avec succès:', registration.scope);
-      })
-      .catch(error => {
-        console.log('Échec de l\'enregistrement du ServiceWorker:', error);
-      });
-  });
-}
-
 // Initialisation de CodeMirror pour chaque éditeur
 const htmlEditor = CodeMirror.fromTextArea(
   document.getElementById("html-editor"),
